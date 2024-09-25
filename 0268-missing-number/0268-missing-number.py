d1de2fd51bdf3eddbc5f4db492b8895ed60b3175
Yetmgeta Ewunetu <120470570+yetmgetaewunetu@users.noreply.github.com>
2024-09-25 15:10:41 +0300

    Time: 115 ms (40.77%), Space: 18.2 MB (11.77%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,14 +1,7 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        length = len(nums)
-        nums.sort()
-        
-        if max(nums) < length:
-            return length
-        elif 0 not in nums:
-            return 0
-        else:
-            for i in range(1, length):
-                if nums[i] - nums[i - 1] == 2:
-                    return  nums[i - 1] + 1
-
+        items = set(range(0, len(nums) + 1))
+        res = -1
+        for num in nums:
+            items.remove(num)
+        return items.pop()
