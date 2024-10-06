6a7908f6c1831a6ffc39eca7f886789af4c0c2a5
Yetmgeta Ewunetu <120470570+yetmgetaewunetu@users.noreply.github.com>
2024-10-06 23:02:44 +0300

    Time: 163 ms (40.88%), Space: 28 MB (71.66%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -4,6 +4,15 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         # l,r = 0, len(nums) -1
-        for i in range(k):
-            nums.insert(0,nums.pop())
+        # for i in range(k):
+        #     nums.insert(0,nums.pop())
+        # return nums
+
+        #using extra space
+        res = nums[:] 
+        print(res)
+        for i,e in enumerate(nums):
+            res[(i+k)%len(nums)] = e
+        for i,e in enumerate(res):
+            nums[i] = e
         return nums
