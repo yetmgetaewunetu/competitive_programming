d01c56f8f8667b45d2bafc5cd5f3db9e2f51af65
Yetmgeta Ewunetu <120470570+yetmgetaewunetu@users.noreply.github.com>
2025-02-14 14:23:56 +0300

    Created 'Longest k-Good Segment' by A2SV Hub

---
+++ 14-Feb-2025/Longest k-Good Segment 265484.py
@@ -0,0 +1,18 @@
+# Problem: Longest k-Good Segment - https://codeforces.com/problemset/problem/616/D
+
+a,b = map(int,input().split())
+arr = list(map(int,input().split()))
+window = {}
+res = [1,1]
+
+left = 0
+for r in range(a):
+    window[arr[r]] = window.get(arr[r],0) + 1
+    while len(window) > b:
+        window[arr[left]] -=1
+        if window[arr[left]] == 0:
+            del window[arr[left]]
+        left += 1
+    if res[-1] - res[0] < r-left:
+        res = [left+1,r+1]
+print(*res)
