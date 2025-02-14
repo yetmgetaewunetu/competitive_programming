e6b1afd1d424c62d50507686c5f27329fe58304d
Yetmgeta Ewunetu <120470570+yetmgetaewunetu@users.noreply.github.com>
2025-02-14 15:02:18 +0300

    Created 'Books' by A2SV Hub

---
+++ 14-Feb-2025/Books 265652.py
@@ -0,0 +1,12 @@
+# Problem: Books - https://codeforces.com/contest/279/problem/B
+
+a,b = map(int,input().split())
+arr = list(map(int,input().split()))
+window = l = res = 0
+for r in range(a):
+    window += arr[r]
+    while window > b:
+        window -= arr[l]
+        l += 1
+    res = max(r-l+1, res)
+print(res)
