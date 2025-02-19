6b9765b83c599cb7d55e0af1b0a3180ec3bda48d
Yetmgeta Ewunetu <120470570+yetmgetaewunetu@users.noreply.github.com>
2025-02-19 09:17:01 +0300

    Created 'Karen and Coffee' by A2SV Hub

---
+++ 19-Feb-2025/Karen and Coffee 272876.py
@@ -0,0 +1,20 @@
+# Problem: Karen and Coffee - https://codeforces.com/contest/816/problem/B
+
+n,k,q = map(int, input().split())
+arr = [0]*(200002)
+for i in range(n):
+    a,b = map(int,input().split())
+    arr[a] += 1
+    arr[b+1] -= 1
+for i in range(1,len(arr)):
+    arr[i] += arr[i-1]
+for i in range(len(arr)):
+    if arr[i] < k:
+        arr[i] = 0
+    else:
+        arr[i] = 1
+for i in range(1,len(arr)):
+    arr[i] += arr[i-1]
+for j in range(q):
+    a,b = map(int,input().split())
+    print(arr[b] - arr[a-1])
