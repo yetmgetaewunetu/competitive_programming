5ee166fef1403fa579d171281bf530ae55c8c1eb
Yetmgeta Ewunetu <120470570+yetmgetaewunetu@users.noreply.github.com>
2025-02-24 14:47:39 +0300

    Created 'Reverse Nodes in k-Group' by A2SV Hub

---
+++ 24-Feb-2025/Reverse Nodes in k-Group 281584.py
@@ -0,0 +1,50 @@
+# Problem: Reverse Nodes in k-Group - https://leetcode.com/problems/reverse-nodes-in-k-group/
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+
+    def reverse_part(self, cur,last):
+        prev = None
+        nx = cur
+        if cur:
+            nx = cur.next
+        while cur != last:
+            cur.next = prev
+            prev = cur
+            cur = nx
+            nx = nx.next
+        cur.next = prev
+        return cur
+
+    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        nodes = []
+        first = head
+        cur = head
+        temp = None
+        dummy = ListNode(None)
+        while cur:
+            for i in range(k):
+                if not cur:
+                    break
+                temp = cur
+                cur = cur.next
+            else:
+                if temp:
+                    f = self.reverse_part(first,temp)
+                    nodes.append(f)
+                    first = cur
+                    continue
+            nodes.append(first)
+        cur = dummy
+        for node in nodes:
+            item = node
+            while item:
+                cur.next = item
+                cur = cur.next
+                item = item.next
+        return dummy.next
+            
