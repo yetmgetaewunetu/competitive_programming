687b8c85e403d09559917d023cc55349ab090460
Yetmgeta Ewunetu <120470570+yetmgetaewunetu@users.noreply.github.com>
2025-02-28 19:41:55 +0300

    Created 'D - Minimize the Distance' by A2SV Hub

---
+++ A2SV G6 - Round #4 28-Feb-2025/D - Minimize the Distance 287143.py
@@ -0,0 +1,9 @@
+# Problem: D - Minimize the Distance - https://codeforces.com/gym/590053/problem/D
+
+t =int(input())
+arr = list(map(int,input().split()))
+arr.sort()
+if not t%2:
+    print(arr[t//2 - 1])
+else:
+    print(arr[t//2])
