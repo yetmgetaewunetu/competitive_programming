6b278a6cf4bfa1d64f50cd2dd566fc38cf7fd664
Yetmgeta Ewunetu <120470570+yetmgetaewunetu@users.noreply.github.com>
2025-03-04 18:55:59 +0300

    Created 'Rabbits in Forest' by A2SV Hub

---
+++ 04-Mar-2025/Rabbits in Forest 291764.py
@@ -0,0 +1,17 @@
+# Problem: Rabbits in Forest - https://leetcode.com/problems/rabbits-in-forest/
+
+class Solution:
+    def numRabbits(self, answers: List[int]) -> int:
+        answers.sort()
+        res = 0
+        rem = 0
+        pos = 0
+        while pos <len(answers):
+            res += answers[pos] + 1
+            j = pos
+            while j-pos <= answers[pos] and answers[j] == answers[pos]:
+                j += 1
+                if j >= len(answers):
+                    return res
+            pos = j
+        return res
