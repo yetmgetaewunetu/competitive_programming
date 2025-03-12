d4944de6cec6abcb9d626d1e797ec96f6d72e154
Yetmgeta Ewunetu <120470570+yetmgetaewunetu@users.noreply.github.com>
2025-03-12 19:52:59 +0300

    Created 'E - Minimum Subsequence' by A2SV Hub

---
+++ A2SV G6 - Round #6 12-Mar-2025/E - Minimum Subsequence 304237.py
@@ -0,0 +1,36 @@
+# Problem: E - Minimum Subsequence - https://codeforces.com/gym/594077/problem/E
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    arr = list(map(int,list(input())))
+    zeros = []
+    ones = []
+    num = 1
+    res = 0
+    for i in range(n):
+        if arr[i] == 0:
+            if ones:
+                arr[i] = ones[-1]
+                ones.pop()
+                zeros.append(arr[i])
+            else:
+                arr[i] = num
+                zeros.append(num)
+                res = num
+                num += 1
+        else:
+            if zeros:
+                arr[i] = zeros[-1]
+                zeros.pop()
+                ones.append(arr[i])
+            else:
+                arr[i] = num
+                ones.append(num)
+                res = num
+                num += 1
+    print(res)
+    print(*arr)
+
+
+
