7196df132d945c02fbc7e40252f3c3eaf642207b
Yetmgeta Ewunetu <120470570+yetmgetaewunetu@users.noreply.github.com>
2025-03-13 18:01:12 +0300

    Time: 0 ms (100%), Space: 17.7 MB (78.9%) - LeetHub

---
+++ 0886-score-of-parentheses/0886-score-of-parentheses.py
@@ -0,0 +1,27 @@
+class Solution:
+    
+    def scoreOfParentheses(self, s: str) -> int:
+        res = 0
+        stack  = []
+        i = 0
+        while i < len(s):
+            if s[i] == '(':
+                if not stack:
+                    stack.append(i)
+                else:
+                    temp = i
+                    while i < len(s) and stack:
+                        if s[i] == '(':
+                            stack.append('(')
+                        else:
+                            stack.pop()
+                        i += 1
+                    i -=1
+                    res += 2 *self.scoreOfParentheses(s[temp:i])
+            else:
+                if stack:
+                    stack.pop()
+                    res += 1
+            i += 1
+        return res
+
