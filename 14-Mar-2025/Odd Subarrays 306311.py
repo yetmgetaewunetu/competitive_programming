53a2d17bedf5848ab48101bfcf161f58aa83d267
Yetmgeta Ewunetu <120470570+yetmgetaewunetu@users.noreply.github.com>
2025-03-14 11:57:05 +0300

    Created 'Odd Subarrays' by A2SV Hub

---
+++ 14-Mar-2025/Odd Subarrays 306311.py
@@ -0,0 +1,16 @@
+# Problem: Odd Subarrays - https://codeforces.com/problemset/problem/1686/B
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    arr = list(map(int,input().split()))
+    res  = 0
+    cur_max = 0
+    for i in arr:
+        if cur_max > i:
+            res +=1
+            cur_max = 0
+        else:
+            cur_max = i
+    print(res)
+        
