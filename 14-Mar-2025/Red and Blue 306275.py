2cc4085c024c9d805a45e5b71dddf6d6f667c564
Yetmgeta Ewunetu <120470570+yetmgetaewunetu@users.noreply.github.com>
2025-03-14 11:22:27 +0300

    Created 'Red and Blue' by A2SV Hub

---
+++ 14-Mar-2025/Red and Blue 306275.py
@@ -0,0 +1,17 @@
+# Problem: Red and Blue - https://codeforces.com/contest/1469/problem/B
+
+t = int(input())
+for _ in range(t):
+    n= int(input())
+    arr = list(map(int,input().split()))
+    m = int(input())
+    arr2 = list(map(int,input().split()))
+    cur1 = cur2 = res1 = res2 = 0
+
+    for i in arr:
+        cur1 += i
+        res1 = max(res1,cur1)
+    for i in arr2:
+        cur2 += i
+        res2 = max(res2,cur2)
+    print(res1 + res2)
