6c98cb20d618d1ff1fcf329dee7304dbd27a62cb
Yetmgeta Ewunetu <120470570+yetmgetaewunetu@users.noreply.github.com>
2025-03-14 11:32:57 +0300

    Created 'Kuriyama Mirai's Stones' by A2SV Hub

---
+++ 14-Mar-2025/Kuriyama Mirai's Stones 306286.py
@@ -0,0 +1,20 @@
+# Problem: Kuriyama Mirai's Stones - https://codeforces.com/contest/433/problem/B
+
+n = int(input())
+arr = list(map(int,input().split()))
+arr2 = arr[:]
+arr2.sort()
+prefix1 = [0]
+prefix2 = [0]
+for i in arr:
+    prefix1.append(prefix1[-1] + i)
+for i in arr2:
+    prefix2.append(prefix2[-1] + i)
+
+m = int(input())
+for i in range(m):
+    c,l,r = map(int,input().split())
+    if c == 1:
+        print(prefix1[r] - prefix1[l-1])
+    else:
+        print(prefix2[r] - prefix2[l-1])
