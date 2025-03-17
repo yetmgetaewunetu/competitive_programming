aad7923aef153bfa8a0560a6fd48a48e27624daf
Yetmgeta Ewunetu <120470570+yetmgetaewunetu@users.noreply.github.com>
2025-03-17 19:02:09 +0300

    Time: 4 ms (18.57%), Space: 19.4 MB (39.97%) - LeetHub

--- 0784-insert-into-a-binary-search-tree/0784-insert-into-a-binary-search-tree.py
+++ 0784-insert-into-a-binary-search-tree/0784-insert-into-a-binary-search-tree.py
@@ -6,20 +6,46 @@
 #         self.right = right
 class Solution:
     def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
-        node= TreeNode(val)
-        dummy = TreeNode(float('inf'))
-        dummy.left = root
-        cur = dummy
-        while cur:
-            if val < cur.val:
-                if not cur.left:
-                    cur.left = node
-                    break
-                cur = cur.left
+        if not root:
+            root = TreeNode(val)
+            return root
+        if val < root.val:
+            if root.left:
+                self.insertIntoBST(root.left,val)
             else:
-                if not cur.right:
-                    cur.right = node
-                    break
-                cur = cur.right
-        return dummy.left
+                root.left = TreeNode(val)
+        else:
+            if root.right:
+                self.insertIntoBST(root.right,val)
+            else:
+                root.right = TreeNode(val)
+        return root
+        
+        # if val < root.val:
+        #     root.left = self.insertIntoBST(root.left,val)
+        # else:
+        #     root.right = self.insertIntoBST(root.right,val)
+        
+        # return root
+
+        
+        
+        
+        
+        # node= TreeNode(val)
+        # dummy = TreeNode(float('inf'))
+        # dummy.left = root
+        # cur = dummy
+        # while cur:
+        #     if val < cur.val:
+        #         if not cur.left:
+        #             cur.left = node
+        #             break
+        #         cur = cur.left
+        #     else:
+        #         if not cur.right:
+        #             cur.right = node
+        #             break
+        #         cur = cur.right
+        # return dummy.left
             
