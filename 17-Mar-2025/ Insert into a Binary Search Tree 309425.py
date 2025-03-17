59602a566f306ce7d1043253b53069e05055b67c
Yetmgeta Ewunetu <120470570+yetmgetaewunetu@users.noreply.github.com>
2025-03-17 18:54:11 +0300

    Created ' Insert into a Binary Search Tree' by A2SV Hub

---
+++ 17-Mar-2025/ Insert into a Binary Search Tree 309425.py
@@ -0,0 +1,27 @@
+# Problem:  Insert into a Binary Search Tree - https://leetcode.com/problems/insert-into-a-binary-search-tree/
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        node= TreeNode(val)
+        dummy = TreeNode(float('inf'))
+        dummy.left = root
+        cur = dummy
+        while cur:
+            if val < cur.val:
+                if not cur.left:
+                    cur.left = node
+                    break
+                cur = cur.left
+            else:
+                if not cur.right:
+                    cur.right = node
+                    break
+                cur = cur.right
+        return dummy.left
+            
